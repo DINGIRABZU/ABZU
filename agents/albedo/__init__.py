fb3d8737c1280bc1291e94fffc2ea9b9cc56eb48
DINGIRABZU <lunaspellstudio@gmail.com>
2025-08-26 22:35:51 +0200

    Add trust update with logging and tests

--- agents/albedo/__init__.py
+++ agents/albedo/__init__.py
@@ -1,5 +1,6 @@
 """Albedo agent messaging utilities."""
 
 from .messaging import compose_message_nazarick, compose_message_rival
+from .trust import update_trust
 
-__all__ = ["compose_message_nazarick", "compose_message_rival"]
+__all__ = ["compose_message_nazarick", "compose_message_rival", "update_trust"]
