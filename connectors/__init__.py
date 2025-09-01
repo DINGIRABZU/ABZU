a0002ab28f4d1555268008f49c2fa4460df30e13
DINGIRABZU <lunaspellstudio@gmail.com>
2025-09-01 20:58:33 +0200

    feat: persist narrative events and expose API

--- connectors/__init__.py
+++ connectors/__init__.py
@@ -8,10 +8,12 @@ from .webrtc_connector import close_peers as webrtc_close_peers
 from .webrtc_connector import router as webrtc_router
 from .webrtc_connector import start_call as webrtc_start_call
 from .primordials_api import send_metrics as primordials_send_metrics
+from narrative_api import router as narrative_router
 
 __all__ = [
     "webrtc_router",
     "webrtc_start_call",
     "webrtc_close_peers",
     "primordials_send_metrics",
+    "narrative_router",
 ]
